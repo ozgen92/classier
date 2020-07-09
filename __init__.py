6c5a2659e18e72e0935995ba2738d0c829ea48a1
Ozgen Eren <ozgeneral@gmail.com>
2020-07-10 00:25:25 +0200

    changes in __init__

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
-import classier.decorators as decorators
-import classier.locks as locks
-import classier.utils as utils
-from classier.objects.PersistentDict import PersistentDict
+from . import decorators
+from . import locks
+from . import utils
+from . import objects
 
+__all__ = [
+    'decorators',
+    'locks',
+    'utils',
+    'objects'
+]
